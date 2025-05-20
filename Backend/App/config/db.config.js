44f4ab1e4415bb0ace926dc388bf6adc1aae19a6
Luis Enrique Campos juarez <LenriqueJ2558gmail.com>
2025-05-20 00:52:04 -0500

    actulizado

--- Backend/App/config/db.config.js
+++ Backend/App/config/db.config.js
@@ -1,10 +1,10 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('dbsgs', 'root', 'sasuke2558', {
   host: 'localhost',
-  dialect: 'mysql', 
-  port:'3306'
+  dialect: 'mysql',
+  port: '3306',
+  timezone: '-05:00', // <-- Esto ajusta la zona horaria a Perú
 });
 
 module.exports = sequelize;
-  
